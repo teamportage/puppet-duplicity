e1f85d79af817c830a73b4b955a19d57ca2feb17
Myles Steinhauser <592187+masteinhauser@users.noreply.github.com>
2022-01-04 22:01:01 -0500

    job: fix cron syntax to capture log output

--- manifests/job.pp
+++ manifests/job.pp
@@ -30,7 +30,7 @@ define duplicity::job(
   } ->
   cron { $script_path:
     ensure  => present,
-    command => "${script_path} >>${log_path} || echo 'Duplicity backup failed'",
+    command => "bash -c \"${shell} ${script_path} >> ${log_path} || echo 'Duplicity backup failed'\"",
     hour    => $hour,
     minute  => $minute
   }
